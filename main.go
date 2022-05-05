050acdf8dd7fe1043f954cbd00df18225e3ffd87
Charles Cabergs <charles.cabergs@colruytgroup.com>
2022-05-05 15:08:57 +0200

    Added a few utility methods on trie, reading system dict into a trie

--- main.go
+++ main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/cacharle/tried/trie"
 )
 
 func main() {
     t := trie.New()
-    t.AddString("bonjour")
-    t.AddString("bonsoir")
-    fmt.Print(t)
+
+    file, err := os.Open("/usr/share/dict/words")
+    if err != nil {
+        log.Fatal(err)
+    }
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        t.Insert(scanner.Text())
+    }
+
+    // t.Insert("bonsoir")
+    // t.Insert("bonjour")
+    // t.Insert("aurevoir")
+    // t.Insert("good")
+    // t.Insert("goodbye")
+    // fmt.Print(t)
+
+    for _, w := range t.AtPrefix("hell").Words() {
+        fmt.Println(w)
+    }
+
+
+
+    // fmt.Println(t.Contains("bonjour"))
 }
--- trie/trie.go
+++ trie/trie.go
@@ -4,18 +4,23 @@ import "fmt"
 
 type Trie struct {
     children map[rune]*Trie
+    end bool
 }
 
 func New() *Trie {
-    return &Trie{ map[rune]*Trie{} }
+    return &Trie{
+        children: map[rune]*Trie{},
+        end: false,
+    }
 }
 
-func (t *Trie) AddString(s string) {
-    t.AddRunes([]rune(s))
+func (t *Trie) Insert(s string) {
+    t.InsertRunes([]rune(s))
 }
 
-func (t *Trie) AddRunes(s []rune) {
+func (t *Trie) InsertRunes(s []rune) {
     if len(s) == 0 {
+        t.end = true
         return
     }
     sub, ok := t.children[s[0]]
@@ -23,7 +28,73 @@ func (t *Trie) AddRunes(s []rune) {
         t.children[s[0]] = New()
         sub = t.children[s[0]]
     }
-    sub.AddRunes(s[1:])
+    sub.InsertRunes(s[1:])
+}
+
+func (t *Trie) Delete(s string) {
+    t.DeleteRunes([]rune(s))
+}
+
+func (t *Trie) DeleteRunes(s []rune) {
+    // TODO:
+    if len(s) == 0 {
+        if t.end {
+            t.end = false
+        }
+        return
+    }
+    t.DeleteRunes(s[1:])
+    if len(t.children) == 1 {
+        delete(t.children, s[0])
+    }
+
+}
+
+func (t *Trie) Contains(s string) bool {
+    return t.ContainsRunes([]rune(s))
+}
+
+func (t *Trie) ContainsRunes(s []rune) bool {
+    if len(s) == 0 && t.end {
+        return true
+    }
+    sub, ok := t.children[s[0]]
+    if !ok {
+        return false
+    }
+    return sub.ContainsRunes(s[1:])
+}
+
+func (t *Trie) wordsRec(current []rune, acc *[]string) {
+    for c, sub := range t.children {
+        word := append(current, c)
+        if sub.end {
+            *acc = append(*acc, string(word))
+        }
+        sub.wordsRec(word, acc)
+    }
+}
+
+func (t *Trie) Words() (acc []string) {
+    t.wordsRec([]rune{}, &acc)
+    return
+}
+
+func (t *Trie) AtPrefix(prefix string) *Trie {
+    return t.AtPrefixRunes([]rune(prefix))
+}
+
+func (t *Trie) AtPrefixRunes(prefix []rune) *Trie {
+    if len(prefix) == 0 {
+        return t
+    }
+    sub, ok := t.children[prefix[0]]
+    if !ok {
+        return nil
+    }
+    prefixed := New()
+    prefixed.children[prefix[0]] = sub.AtPrefixRunes(prefix[1:])
+    return prefixed
 }
 
 func (t *Trie) String() string {
@@ -34,7 +105,12 @@ func (t *Trie) String() string {
             for i := 0; i < depth; i++ {
                 ret += "  "
             }
-            ret += fmt.Sprintf("%c\n", k)
+            if v.end {
+                ret += fmt.Sprintf("%c*\n", k)
+
+            } else {
+                ret += fmt.Sprintf("%c \n", k)
+            }
             rec(v, depth + 1)
         }
     }
